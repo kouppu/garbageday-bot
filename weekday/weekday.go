6790b1a680dd78ea71768946571bb88895ac0647
Kou <suhrr.code@gmail.com>
2022-05-07 10:12:10 +0900

    Refactor weekday.go
    - CountWeekdayInMonth

--- weekday/weekday.go
+++ weekday/weekday.go
@@ -6,18 +6,21 @@ import (
 
 // 引数の日付が、月の何回目の曜日かを返す
 func CountWeekdayInMonth(target time.Time) int {
+	// 月末日を取得
 	eom := time.Date(target.Year(), target.Month(), 1, 0, 0, 0, 0, target.Location()).AddDate(0, 1, -1)
 
-	count := 1
+	weekdayInMonth := 1
 	for i := 1; i <= eom.Day(); i++ {
 		t := time.Date(target.Year(), target.Month(), i, 0, 0, 0, 0, target.Location())
 		if target.Day() == t.Day() {
 			break
 		}
-		if t.Day()%7 == 0 {
-			count++
+		if t.Day()%7 != 0 {
+			continue
 		}
+
+		weekdayInMonth++
 	}
 
-	return count
+	return weekdayInMonth
 }
